e32a89e239bbf47f0dba8317889a54ea1f65561d
Mark Leo <ratrateroo@gmail.com>
2020-01-22 20:44:40 +0800

    updated App.js

--- react-calendar-app/src/App.js
+++ react-calendar-app/src/App.js
@@ -1,26 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from "react";
+import { Router, Route } from "react-router-dom";
+import HomePage from "./HomePage";
+import { createBrowserHistory as createHistory } from "history";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import "./App.css";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+import "react-datepicker/dist/react-datepicker.css";
+const history = createHistory();
+function App({ calendarStore }) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router history={history}>
+        <Navbar bg="primary" expand="lg" variant="dark">
+          <Navbar.Brand href="#home">Calendar App</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mr-auto">
+              <Nav.Link href="/">Home</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+        <Route
+          path="/"
+          exact
+          component={props => (
+            <HomePage {...props} calendarStore={calendarStore} />
+          )}
+        />
+      </Router>
     </div>
   );
 }
-
 export default App;
